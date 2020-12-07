d9a4e4a2ce2efc8c45f71e294c77c18cc6678932
Luis Felipe Klaus <duckbox@hotmail.com>
2020-12-07 20:40:33 -0300

    paginator html initial setup

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -50,7 +50,7 @@ function loadFeed(feed) {
             document.querySelector("#feed-view-no"). innerHTML =
             `<div class="row my-text mt-3 justify-content-center"
                 <div class="col-6  text-center">
-                    This feed has no posts yet.
+                    You are not following anyone.
                 </div>
             </div>`;
         };
@@ -59,6 +59,29 @@ function loadFeed(feed) {
         posts.forEach(post => {
             generatePost(post, feed)
         });
+
+        document.querySelector("#feed-view-paginator"). innerHTML =
+        `<div class="row small justify-content-center mt-3">
+            <nav aria-label="Feed Navigation">
+                <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link my-text my text-hover" href="#" aria-label="Previous">
+                    <span aria-hidden="true"><i class="fas fa-angle-double-left"></i></span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link  my-text my text-hover" href="#">1</a></li>
+                <li class="page-item"><a class="page-link  my-text my text-hover" href="#">2</a></li>
+                <li class="page-item"><a class="page-link  my-text my text-hover" href="#">3</a></li>
+                    <a class="page-link my-text my text-hover" href="#" aria-label="Next">
+                    <span aria-hidden="true"><i class="fas fa-angle-double-right"></i></span> 
+                    </a>
+                </li>
+                </ul>
+            </nav>
+        </div>`
+
+
+
     })
 
 }
